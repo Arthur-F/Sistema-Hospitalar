c82bb705e494c325967f37171be599dbfd395d00
Arthur Ferreira <arthurferreira2602@gmail.com>
2018-06-27 21:12:30 -0300

    Revert "Merge origin/testbranch into HEAD"
    This reverts commit 1a558c9189ff2f0f59c660f8a2d4e4e4c55e5762.

--- Sistema Hospitalar/src/sistema/hospitalar/Receita.java
+++ Sistema Hospitalar/src/sistema/hospitalar/Receita.java
@@ -3,8 +3,7 @@ package sistema.hospitalar;
 public class Receita {
 
     private String receita;
-    private String tipo;
-    private Long cpfpaciente;
+
     public String getReceita() {
         return receita;
     }
@@ -12,22 +11,6 @@ public class Receita {
     public void setReceita(String receita) {
 
         this.receita = receita;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public Long getCpfpaciente() {
-        return cpfpaciente;
-    }
-
-    public void setCpfpaciente(Long cpfpaciente) {
-        this.cpfpaciente = cpfpaciente;
     }
 
 }
